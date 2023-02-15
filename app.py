3d909e193d6e0a2a3532bcbc0404b9f109b83b17
Hevsy <roomyn@gmail.com>
2023-02-15 17:46:37 +1100

    2

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, render_template, request, session
+from sqlalchemy import Engine
 
 # Configure application
 app = Flask(__name__)
@@ -11,9 +12,57 @@ def after_request(response):
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# # import db config from etc/config
+# from etc.config import db_config
+# db_type = None
+# db_file = None
+# db_username = None
+# db_pass = None 
+# db_host = None
+
+# # Assign variables from db_config
+# for item in db_config:
+#     exec('{KEY} = {VALUE}'.format(KEY = item, VALUE = repr(db_config[item])))
+
+# # Create db URL & engine
+# db_url = URL.create(db_type, database = db_file, username = db_username, password = db_pass, host = db_host)
+# engine = create_engine(db_url)
     
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
+@ app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+        if not username or not password or not confirmation:
+            return apology("Must provide username and password!")
+        elif password != confirmation:
+            return apology("Passwords do not match!")
+
+        # Check if username already exists
+        with engine.begin() as db:
+            result = db.execute(text('SELECT id FROM users WHERE username = :u'), {'u': username})
+            print (result.rowcount)
+            if result.rowcount > 0:
+                # return apology("Username already exists")
+                return apology("Username already exists")
+            else:
+                hash = generate_password_hash(password)
+                db.execute(text(
+                    "INSERT INTO users (username, hash) VALUES (:u, :h)"), {"u": username, "h": hash})
+                return redirect("/login")
---
+++ etc/config.py
@@ -0,0 +1,28 @@
+# import boto3
+
+# def param_get(param_name):
+#     # Create a client for the SSM service
+#     ssm = boto3.client('ssm', region_name='us-east-1')
+
+#     # Get the value of the parameter
+#     return (ssm.get_parameter(Name=param_name, WithDecryption=True))['Parameter']['Value']
+
+
+db_config = {
+    "db_type" : "postgresql+psycopg2", 
+    "db_username" : "postgres", 
+    "db_pass" : "postgres"
+    "db_host" : "db-finance1.cxhmztea6vcv.us-east-1.rds.amazonaws.com", 
+    "db_file" :"finance"
+}
+
+API_KEY = param_get('finance_api-key')
+# print (API_KEY, db_config)
+
+db_config_test = {
+    "db_type" : "sqlite", 
+    "db_username" : None, 
+    "db_pass" : None, 
+    "db_host" : None, 
+    "db_file" :"finance.db"
+}
