59cba3f7d189817cbe2a1b5ac4ee15d0523e860e
Hevsy <roomyn@gmail.com>
2023-02-21 17:02:37 +1100

    db-init works; github actions test

--- db_init.py
+++ db_init.py
@@ -1,10 +1,12 @@
 from sqlalchemy import URL, create_engine, MetaData, Table, Column, Integer, String
 from sqlalchemy_utils import database_exists, create_database
 from etc.config import db_config
+import _mysql_connector   
+
 
 def db_init():
     # Configure DB connection
-# Import db config from etc/config
+    # Import db config from etc/config
     db_type = ""
     db_file = db_username = db_pass = db_host = None
 # Assign variables from db_config
@@ -12,7 +14,7 @@ def db_init():
         exec('{KEY} = {VALUE}'.format(KEY=item, VALUE=repr(db_config[item])))
 # Create db URL & engine
     db_url = URL.create(db_type, database=db_file, username=db_username,
-                    password=db_pass, host=db_host)
+                        password=db_pass, host=db_host)
     engine = create_engine(db_url)
 # Initialise database
     if not database_exists(engine.url):
@@ -23,12 +25,12 @@ def db_init():
     meta = MetaData()
 # Define tables
     users_table = Table(
-    "users",
-    meta,
-    Column("id", Integer, primary_key=True),
-    Column("username", String(30)),
-    Column("hash", String(60)),
-)
+        "users",
+        meta,
+        Column("id", Integer, primary_key=True),
+        Column("username", String(30)),
+        Column("hash", String(60)),
+    )
 # Create tables
     meta.create_all(engine, checkfirst=True)
-    return engine,users_table
+    return engine, users_table
