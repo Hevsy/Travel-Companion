49a66352cfe61e6ea36df316315411f1b57301a9
Hevsy <roomyn@gmail.com>
2023-05-07 22:18:48 +1000

    updating config and requirements

--- app/etc/config.py
+++ app/etc/config.py
@@ -7,20 +7,34 @@
 
 #     # Get the value of the parameter
 #     return (ssm.get_parameter(Name=param_name, WithDecryption=True))['Parameter']['Value']
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
-db_config = {
-    "type": "mysql+mysqldb",
-    "username": "root",
-    "pass": "root",
-    "host": "localhost",
-    "db": "project-tc",
-}
+match os.getenv("STAGE"):
+    case "DEV":
+        db_config = {
+            "type": "sqlite",
+            "username": None,
+            "pass": None,
+            "host": None,
+            "db": "project-tc.db",
+        }
+    case "TEST":
+        db_config = {
+            "type": "mysql+mysqldb",
+            "username": "root",
+            "pass": "root",
+            "host": "localhost",
+            "db": "project-tc",
+        }
+    case "PROD":
+        db_config = {
+            "type": "mysql+mysqldb",
+            "username": "root",
+            "pass": "root",
+            "host": "localhost",
+            "db": "project-tc",
+        }
 
-db_config_test = {
-    "type": "sqlite",
-    "username": None,
-    "pass": None,
-    "host": None,
-    "db": "project-tc.db",
-}
